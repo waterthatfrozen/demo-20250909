b026f39abf517c62740a20be683df1eb6a0ef258
Paphana Yiwsiw <paphana.yiwsiw@gmail.com>
2025-09-10 00:54:57 +0700

    Demo

---
+++ swaggerDef.js
@@ -0,0 +1,51 @@
+// swaggerDef.js
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+    title: 'Demo BEM API Documentation',
+    version: '1.0.0',
+    description: 'Documentation for Demo BEM API',
+    },
+    components: {
+        schemas: {
+            IntroResponse: {
+                type: 'object',
+                properties: {
+                    message: { type: 'string', example: 'Hello world, this is API demo for dummies' },
+                    demoPath: { type: 'string', example: 'try to use /api/v1/demo/hello-world' },
+                    currentTime: { type: 'integer', format: 'int64', example: 1717449600000 }
+                },
+                required: ['message', 'demoPath', 'currentTime']
+            },
+            HelloWorldResponse: {
+                type: 'object',
+                properties: {
+                    message: { type: 'string', example: 'custom message from BEM' },
+                    data: { type: 'object' }
+                },
+                required: ['message', 'data']
+            },
+            ErrorResponse: {
+                type: 'object',
+                properties: {
+                    message: { type: 'string', example: 'failed by others' },
+                    error: { type: 'object' }
+                },
+                required: ['message']
+            }
+        }
+    },
+    servers: [
+    {
+        url: 'http://localhost:3500/api', // Replace with your API base URL
+        description: 'Development server',
+    },
+    ],
+};
+
+const options = {
+    swaggerDefinition,
+    apis: ['./index.js'], // Path to your API route files
+};
+
+module.exports = options;
